28317df1e66c41dedeaddb1f659af46e38e8c9bb
andrewaclark248 <andrewaclark248@gmail.com>
2023-06-25 15:18:11 -0400

    various chagnes

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-api-dev2.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone JWT_SECRETrelated error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
